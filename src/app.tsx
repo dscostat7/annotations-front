b40acd1d937c59462234edaefff0f13fdb16489e
Diego Souza <diegosouzagestor@gmail.com>
2024-09-10 19:35:19 -0300

    Created home page, functionalities, modal

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,16 @@
+
+import { Dialog } from './components/ui/dialog';
+import { CreateGoal } from './components/create-goal';
+import { Summary } from './components/summary';
+// import { EmptyGoals } from './components/empty-goals';
+
 export function App() {
-  return <h1 className="bg-black text-white text-3xl">Hello World</h1>
+  return (
+    <Dialog>
+      {/* <EmptyGoals /> */}
+      <Summary />
+      
+      <CreateGoal />
+    </Dialog>
+  )
 }
---
+++ src/components/annotation-icon.tsx
@@ -0,0 +1,96 @@
+export function AnnotationIcon() {
+    return (
+        <svg
+      width={36}
+      height={36}
+      viewBox="0 0 36 36"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+        <title>Annotation</title>
+      <path
+        d="M18 0c-1.404 0-2.77.16-4.08.465A18.023 18.023 0 00.465 13.92 18.021 18.021 0 000 18c0 9.941 8.059 18 18 18 1.404 0 2.77-.16 4.081-.465a18.025 18.025 0 0013.454-13.453A18.03 18.03 0 0036.002 18C36 8.059 27.94 0 18 0zm0 29.272c-1.329 0-2.603-.23-3.787-.651a11.304 11.304 0 01-6.833-6.834A11.246 11.246 0 016.728 18c0-6.226 5.046-11.272 11.272-11.272 1.329 0 2.603.23 3.787.652a11.305 11.305 0 016.834 6.833c.422 1.183.651 2.458.651 3.787 0 6.226-5.046 11.272-11.272 11.272z"
+        fill="url(#paint0_linear_1_21)"
+      />
+      <path
+        d="M18 10.735a7.266 7.266 0 100 14.531 7.266 7.266 0 000-14.531zm0 11.658a4.393 4.393 0 110-8.786 4.393 4.393 0 010 8.786z"
+        fill="#FEFCFE"
+      />
+      <path
+        d="M21.787 7.38A11.242 11.242 0 0018 6.728C11.774 6.728 6.728 11.774 6.728 18c0 1.329.23 2.604.652 3.787a11.136 11.136 0 01-3.945-2.552 11.112 11.112 0 01-2.97-5.316A18.023 18.023 0 0113.92.465c1.947.46 3.795 1.45 5.315 2.97a11.134 11.134 0 012.552 3.945z"
+        fill="url(#paint1_linear_1_21)"
+      />
+      <path
+        d="M35.535 22.082a18.025 18.025 0 01-13.454 13.453 11.113 11.113 0 01-5.315-2.97 11.138 11.138 0 01-2.552-3.944c1.183.422 2.457.651 3.786.651 6.226 0 11.272-5.046 11.272-11.272 0-1.329-.23-2.604-.651-3.787a11.139 11.139 0 013.944 2.552 11.117 11.117 0 012.97 5.317z"
+        fill="url(#paint2_linear_1_21)"
+      />
+      <path
+        d="M9.096 30.671a3.767 3.767 0 100-7.534 3.767 3.767 0 000 7.534z"
+        fill="url(#paint3_linear_1_21)"
+      />
+      <path
+        d="M26.937 12.83a3.767 3.767 0 100-7.533 3.767 3.767 0 000 7.534z"
+        fill="url(#paint4_linear_1_21)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_1_21"
+          x1={30.5556}
+          y1={6.13588}
+          x2={5.83534}
+          y2={29.4953}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#F38856" />
+          <stop offset={0.23} stopColor="#F0625C" />
+          <stop offset={1} stopColor="#5C3DAB" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_1_21"
+          x1={16.8608}
+          y1={3.05155}
+          x2={2.79985}
+          y2={17.0368}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#F67986" />
+          <stop offset={1} stopColor="#8951BA" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_1_21"
+          x1={32.9424}
+          y1={19.2195}
+          x2={18.8815}
+          y2={33.2055}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#F67986" />
+          <stop offset={1} stopColor="#8951BA" />
+        </linearGradient>
+        <linearGradient
+          id="paint3_linear_1_21"
+          x1={11.8467}
+          y1={24.3306}
+          x2={6.34537}
+          y2={29.4779}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#A267B9" />
+          <stop offset={1} stopColor="#704BB4" />
+        </linearGradient>
+        <linearGradient
+          id="paint4_linear_1_21"
+          x1={29.2269}
+          y1={6.92033}
+          x2={25.4712}
+          y2={10.4351}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#F7A8A5" />
+          <stop offset={0.97} stopColor="#D074C4" />
+          <stop offset={1} stopColor="#CF76A9" />
+        </linearGradient>
+      </defs>
+    </svg>
+    )
+}
